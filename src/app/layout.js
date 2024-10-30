ff541fe5662cb0b07bd0a3bccd4bacbf43a4974d
Richard Winkelaar <richard@adcalls.nl>
2024-10-30 11:24:33 +0100

    Done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
+  const date = new Date;
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>{date.toLocaleString()}</footer>
       </body>
     </html>
   );
